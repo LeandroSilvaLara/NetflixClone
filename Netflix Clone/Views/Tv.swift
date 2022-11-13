a8f0f26d0da7b46d8119bfdce0deb0c3b7bb2bae
Leandro Lara <leandrodasilvalara@yahoo.com.br>
2022-11-13 08:25:43 -0800

    ajuste

---
+++ Netflix Clone/Views/Tv.swift
@@ -0,0 +1,16 @@
+//
+//  Tv.swift
+//  Netflix Clone
+//
+//  Created by Leandro Lara on 13/11/22.
+//
+
+import Foundation
+
+struct TrendingTvResponse: Codable {
+    let results: [Tv]
+}
+
+struct Tv: Codable {
+    
+}
