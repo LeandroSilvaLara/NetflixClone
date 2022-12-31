61ab50bac185987eff840e9dcf5835bf177ffefb
Leandro Lara <leandrodasilvalara@yahoo.com.br>
2022-12-30 22:40:59 -0800

    Fazendo teste api Movies

--- Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -63,8 +63,14 @@ class HomeViewController: UIViewController {
             //}
         //}
         
-        APICaller.shared.getTrendingTvs { results in
+        //APICaller.shared.getTrendingTvs { results in
             //
+        //}
+       //APICaller.shared.getPopular { _ in
+            //
+        //}
+        APICaller.shared.getTopRated { _ in
+            
         }
     }
 }
