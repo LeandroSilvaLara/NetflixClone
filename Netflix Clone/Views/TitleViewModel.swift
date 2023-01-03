3a391c23dd7970853dd15011c995561ef5d0ad92
Leandro Lara <leandrodasilvalara@yahoo.com.br>
2023-01-03 13:14:04 -0800

    SearchViewController

---
+++ Netflix Clone/Views/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by Leandro Lara on 03/01/23.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
